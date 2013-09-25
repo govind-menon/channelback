09869011da3a5c855cf4fdd57fe03d9e851a418e
Go Wind <govindappumenon@gmail.com>
2013-09-25 15:05:24 -0400

    #tests | Govind | unit tests of comment votes model.Tests are actually catching stuff.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,11 @@ class Comment < ActiveRecord::Base
   attr_accessible :text
   belongs_to :post
   belongs_to :user
+
   validates :text,:presence => true
   validates :post,:presence => true
   validates :user,:presence => true
+
+  has_many :comment_votes
+
 end
--- app/models/comment_vote.rb
+++ app/models/comment_vote.rb
@@ -1,7 +1,10 @@
 class CommentVote < ActiveRecord::Base
   belongs_to :comment
   belongs_to :user
+
   validates_uniqueness_of :id, :scope => [:comment_id,:user_id]
+  validates :comment,:presence => true
+  validates :user,:presence => true
 
   def self.find_vote(comment_id,user_id)
    CommentVote.where(:comment_id => comment_id,:user_id => user_id)
