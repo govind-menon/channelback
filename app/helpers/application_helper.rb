97069d6a02930672de1e718baeb50ea0e644af2f
Ankit <ankit.bhutani7@gmail.co>
2013-09-27 19:38:59 -0400

    #Admin|Ankit|Made some changes to admin homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def is_admin_page?
-    (request.url.to_s.include? '/admin' or request.url.to_s.include? '/category')
+    (request.url.to_s.include? '/admin' or request.url.to_s.include? '/categories')
     end
   end
